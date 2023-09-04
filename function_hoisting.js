1815180833088b4da6c1c0afc554803e00330f73
Raj kishan <95044393+xostar2@users.noreply.github.com>
2023-09-05 01:05:14 +0530

    Create function_hoisting.js

---
+++ function_hoisting.js
@@ -0,0 +1,21 @@
+
+//----------Hoisting in js-----------
+//calling a  function and using a variable before declareing it is called hoisting
+let x=7;
+ 
+function getname(){
+	console.log("namste javastript");
+}
+getname();
+console.log(x);
+console.log(getname)
+
+var getnaam=()=>{
+	console.log("ram ram sareya ne ")
+}
+
+var setname=function(){
+	console.log ('another way to initialize function')
+}
+getnaam();
+setname();
