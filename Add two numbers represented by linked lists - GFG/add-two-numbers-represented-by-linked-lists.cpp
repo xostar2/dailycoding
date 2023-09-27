8b09d286dfc37c4d5d2b78dd25d30daecbbdc969
Raj kishan <95044393+xostar2@users.noreply.github.com>
2023-09-27 13:43:16 +0530

    Added solution - LeetHub

---
+++ Add two numbers represented by linked lists - GFG/add-two-numbers-represented-by-linked-lists.cpp
@@ -0,0 +1,178 @@
+//{ Driver Code Starts
+// driver
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Linked list Node */
+struct Node {
+    int data;
+    struct Node* next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+struct Node* buildList(int size)
+{
+    int val;
+    cin>> val;
+    
+    Node* head = new Node(val);
+    Node* tail = head;
+    
+    for(int i=0; i<size-1; i++)
+    {
+        cin>> val;
+        tail->next = new Node(val);
+        tail = tail->next;
+    }
+    
+    return head;
+}
+
+void printList(Node* n)
+{
+    while(n)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+    cout<< endl;
+}
+
+
+// } Driver Code Ends
+/* node for linked list:
+
+struct Node {
+    int data;
+    struct Node* next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+*/
+
+
+class Solution
+{
+    private:
+    Node* reverse(Node* head){
+        Node* cur=head;
+        Node* prev=NULL;
+        Node* fd=NULL;
+        while(cur!=NULL){
+            fd=cur->next;
+            cur->next=prev;
+            prev=cur;
+            cur=fd;
+        }
+        return prev;
+    }
+    
+    void insertAtTail(Node* &head,Node* &Tail,int val){
+        
+        Node* temp=new Node(val);
+        
+        if(head==NULL){//empty list case
+            head=temp;
+            Tail=temp;
+            return;
+        }
+        else{//es time pe list emty nhi hn
+            Tail->next=temp;
+            Tail=temp;
+        }
+    }
+    
+    Node* add(Node* first,Node* second){
+        int carry=0;
+        
+        Node* ansHead=NULL;
+        Node* ansTail=NULL;
+        
+        while(first!=NULL&&second!=NULL){
+            int sum= carry+first->data+second->data;
+            int digit=sum%10;
+            //cerate node and add it in ans linked list
+            insertAtTail(ansHead,ansTail,digit);
+            
+            carry=sum/10;
+            
+            first=first->next;
+            second=second->next;
+        }
+        
+        while(first!=NULL){
+            int sum=carry+first->data;
+            int digit=sum%10;
+            insertAtTail(ansHead,ansTail,digit);
+            carry=sum/10;
+            
+            first=first->next;
+        }
+        
+        while(second!=NULL){
+            int sum=carry+second->data;
+            int digit=sum%10;
+            insertAtTail(ansHead,ansTail,digit);
+            carry=sum/10;
+            
+            second=second->next;
+        }
+        
+        while(carry!=0){
+            int sum=carry;
+            int digit=sum%10;
+            insertAtTail(ansHead,ansTail,digit);
+            carry=sum/10;
+        }
+        return ansHead;
+    }
+    public:
+    //Function to add two numbers represented by linked list.
+    struct Node* addTwoLists(struct Node* first, struct Node* second)
+    {
+        //step1---reverse the input ll
+        first=reverse(first);
+        second=reverse(second);
+        
+        //step2-- addition
+        Node* ans=add(first,second);
+        
+        //step3-- reverse the ans ll
+        ans=reverse(ans);
+        
+        return ans;
+    }
+};
+
+
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, m;
+        
+        cin>>n;
+        Node* first = buildList(n);
+        
+        cin>>m;
+        Node* second = buildList(m);
+        Solution ob;
+        Node* res = ob.addTwoLists(first,second);
+        printList(res);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
